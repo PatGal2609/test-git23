c5bc7ce8db3087254306653b5b72e66c4dc5781a
Freddy <paterson.nya@gmail.com>
2023-10-20 21:01:56 +0300

    обавили проверка на пробелы

--- js/main.js
+++ js/main.js
@@ -46,8 +46,8 @@ const a = prompt('Один из последних просмотренных ф
 
 function rememberMyFilms() {
     for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
-              b = prompt('На сколько оцените его?', '');
+        const a = prompt('Один из последних просмотренных фильмов?', '').trim(),
+              b = +prompt('На сколько оцените его?', '');
         if ( a != null && b != null && a != '' & b != '' && a.length < 50) {
             personalMovieDB.movies[a] = b;
             console.log('Done');
